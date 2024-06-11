08120c68a3974b091fcaa3c8aea36935bb8b4eaf
orvizz <153991700+orvizz@users.noreply.github.com>
2024-06-11 12:12:16 +0200

    middleware for user authentication

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
-
+const jwt = require("jsonwebtoken")
+let activeApiKeys = require("./activeApiKeys")
 const app = express();
 const port = 3000;
 app.use(express.json())
@@ -7,6 +8,27 @@ app.use(express.json())
 let routerUsers = require("./routers/routerUsers")
 let routerItems = require("./routers/routerItems")
 let routerBids = require("./routers/routerBids")
+
+app.use(["/items","/bids"], (req, res, next) => {
+    console.log("middleware execution")
+
+    let apiKey = req.query.apiKey;
+    if( apiKey == undefined ){
+        return res.status(401).json({ error: "no apiKey" })
+    }
+    let infoApiKey;
+    try{
+        infoApiKey = jwt.verify(apiKey,"secret")
+    } catch ( e ) {
+        return res.status(401).json({ error: "invalid apiKey" })
+    }
+    if( infoApiKey == undefined || activeApiKeys.indexOf(apiKey) == -1){
+        return res.status(401).json({ error: "invalid apiKey" })
+    }
+
+    req.infoApiKey = infoApiKey;
+    next();
+})
 
 app.use("/users",routerUsers)
 app.use("/items",routerItems)
