c233136876c3aaf32f04e170ca742442197be5c0
orvizz <153991700+orvizz@users.noreply.github.com>
2024-06-11 11:45:13 +0200

    login with apiKeys and active apiKeys wprking

---
+++ activeApiKeys.js
@@ -0,0 +1,3 @@
+let activeApiKeys = [];
+
+module.exports = activeApiKeys
--- routers/routerUsers.js
+++ routers/routerUsers.js
@@ -1,6 +1,60 @@
 const express = require("express")
 const database = require("../database")
+
+const jwt = require("jsonwebtoken");
+
+
 const routerUsers = express.Router();
+
+const activeApiKeys = require("../activeApiKeys")
+
+routerUsers.post("/login", async (req,res) => {
+    let email = req.body.email
+    let password = req.body.password
+
+    let errors = [];
+    if( email==undefined ){
+        errors.push({error: "No email in body"})
+    }
+    if( password==undefined ){
+        errors.push({error: "No password in body"})
+    }
+
+    if(errors.length > 0) {
+        return res.status(400).json({errors: errors})
+    }
+
+    database.connect();
+    let selectedUsers;
+    try{
+        let selectedUsers = await database.query(
+            "SELECT id, email FROM users WHERE email = ? AND password = ?",
+        [email,password])
+        database.disconnect();
+        
+        if( selectedUsers.length == 0){
+            return res.status(401).json({error: "invalid email or password"})
+        }
+
+        let apiKey = jwt.sign({
+                email: email,
+                id: selectedUsers[0].id,
+                time: Date.now()
+        }, "secret")
+
+        activeApiKeys.push(apiKey)
+
+        res.json({
+            apiKey: apiKey,
+            id: selectedUsers[0].id,
+            email: selectedUsers[0].email,
+        })
+
+    }catch ( e ){
+        database.disconnect();
+        return res.status(400).json({error: "error in login"})
+    }
+})
 
 routerUsers.post("/", async (req,res) => {
     let email = req.body.email
